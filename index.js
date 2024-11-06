dab7e3c8506921728f8c05924d51d5b8a2c8cd71
Om Asanani <asananiom27@gmail.com>
2024-11-06 23:03:30 +0530

    2st

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const net = require("net");
 
-const server = net.createServer((socket) => {});
+const port = 8080;
+const server = net.createServer((socket) => {
+  console.log("Client Connected");
+});
+
+server.listen(port, "localhost", () => {
+  console.log(`listening to port ${port}`);
+});
